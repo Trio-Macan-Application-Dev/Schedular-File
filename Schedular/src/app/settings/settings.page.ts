e5348860dce14956cc0f8d27d74303cb35e7a2e2
mhafizkn98 <hafizkn98@gmail.com>
2021-01-08 21:55:53 +0800

    Display current user data

--- Schedular/src/app/settings/settings.page.ts
+++ Schedular/src/app/settings/settings.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserLoginService } from '../services/user-login.service';
 import { Router } from '@angular/router';
+import {StorageHelperService} from '../services/storage-helper.service'; // import package from StorageHelperService
 
 @Component({
   selector: 'app-settings',
@@ -12,7 +13,23 @@ export class SettingsPage implements OnInit {
   constructor(
     private userLoginService: UserLoginService,
     private route: Router,
+    private storageHelperService: StorageHelperService // invoke pckge
   ) { }
+
+  // Parse value to settings.page.html
+  name = this.storageHelperService.getFullName();
+  //course = this.storageHelperService.getCourseCode();
+  matrik = this.storageHelperService.getNoMatrik();
+  
+  // value sesi semester
+  sesi: String = this.storageHelperService.getCurrentSesi();
+  semester: String = this.storageHelperService.getCurrentSemester();
+  sesiSemester = this.semester + ' | ' + this.sesi ;
+  
+  // value sesi semester start and end
+  semStartDate: String = this.storageHelperService.getStartDate();
+  semEndDate: String = this.storageHelperService.getEndDate();
+  semDate = this.semStartDate + ' till ' + this.semEndDate;
 
   ngOnInit() {
   }
