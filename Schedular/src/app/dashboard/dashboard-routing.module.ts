7be386314142f125525c6e50a4d46a475a38dad6
mhafizkn98 <hafizkn98@gmail.com>
2021-01-11 00:40:43 +0800

    Update navigate in dashboard page
    For timetable and event

--- Schedular/src/app/dashboard/dashboard-routing.module.ts
+++ Schedular/src/app/dashboard/dashboard-routing.module.ts
@@ -2,12 +2,54 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardPage } from './dashboard.page';
+import { TimetablePage } from './timetable/timetable.page';
+import { EventtablePage } from './eventtable/eventtable.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: DashboardPage
+    component: DashboardPage,
+    children: [
+      {
+        path: 'time-table',
+        children: [
+          {
+            path: '',
+            component: TimetablePage,
+          }
+        ]
+      },
+      {
+        path: 'event-table',
+        children: [
+          {
+            path: '',
+            component: EventtablePage,
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard/time-table',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'timetable',
+    loadChildren: () => import('./timetable/timetable.module').then( m => m.TimetablePageModule)
+  },
+  {
+    path: 'eventtable',
+    loadChildren: () => import('./eventtable/eventtable.module').then( m => m.EventtablePageModule)
   }
+
+
 ];
 
 @NgModule({
