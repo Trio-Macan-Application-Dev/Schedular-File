9abf202b01a831f268896954c15188aa8f702ef3
Hafiz <hafizkn98@gmail.com>
2021-02-04 17:02:38 +0800

    Settings & Lecturer

--- Schedular/src/app/lecturer/lecturer-routing.module.ts
+++ Schedular/src/app/lecturer/lecturer-routing.module.ts
@@ -8,46 +8,46 @@ import { StudLecturerListPage } from './stud-lecturer-list/stud-lecturer-list.pa
 const routes: Routes = [
   {
     path: '',
-    component: LecturerPage,
-    children: [
-      {
-        path: 'stud-lecturer',
-        children: [
-          {
-            path: '',
-            component: StudLecturerListPage,
-          }
-        ]
-      },
-      {
-        path: 'all-lecturer',
-        children: [
-          {
-            path: '',
-            component: AllLecturerListPage,
-          }
-        ]
-      },
-      {
-        path: '',
-        redirectTo: '/lecturer/all-lecturer',
-        pathMatch: 'full'
-      }
-    ]
-  },
-  {
-    path: '',
-    redirectTo: '/lecturer',
-    pathMatch: 'full'
-  },
-  {
-    path: 'all-lecturer-list',
-    loadChildren: () => import('./all-lecturer-list/all-lecturer-list.module').then( m => m.AllLecturerListPageModule)
-  },
-  {
-    path: 'stud-lecturer-list',
-    loadChildren: () => import('./stud-lecturer-list/stud-lecturer-list.module').then( m => m.StudLecturerListPageModule)
+    component: AllLecturerListPage,
+    // children: [
+    //   {
+    //     path: 'stud-lecturer',
+    //     children: [
+    //       {
+    //         path: '',
+    //         component: StudLecturerListPage,
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     path: 'all-lecturer',
+    //     children: [
+    //       {
+    //         path: '',
+    //         component: AllLecturerListPage,
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     path: '',
+    //     redirectTo: '/lecturer/all-lecturer',
+    //     pathMatch: 'full'
+    //   }
+    // ]
   }
+  // {
+  //   path: '',
+  //   redirectTo: '/all-lecturer',
+  //   pathMatch: 'full'
+  // },
+  // {
+  //   path: 'all-lecturer-list',
+  //   loadChildren: () => import('./all-lecturer-list/all-lecturer-list.module').then( m => m.AllLecturerListPageModule)
+  // },
+  // {
+  //   path: 'stud-lecturer-list',
+  //   loadChildren: () => import('./stud-lecturer-list/stud-lecturer-list.module').then( m => m.StudLecturerListPageModule)
+  // }
 
 ];
 
